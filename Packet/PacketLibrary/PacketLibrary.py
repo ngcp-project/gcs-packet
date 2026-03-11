e69d5aa2f7a66f6626352d8cb77977aa9e9aeba7
Feniren <sanders.aidan.c.ss@gmail.com>
2026-03-10 23:13:34 -0700

    Updated PacketLibrary

--- Packet/PacketLibrary/PacketLibrary.py
+++ Packet/PacketLibrary/PacketLibrary.py
@@ -1,13 +1,29 @@
 from Enum import *
 
-@staticmethod
-def GetVehicleFromMACAddress(MACAddress: str) -> Vehicle:
-    match(MACAddress):
-        case "1":
+class PacketLibrary:
+    MRA_MAC_ADDRESS = ""
+    MEA_MAC_ADDRESS = ""
+    ERU_MAC_ADDRESS = ""
+
+    @staticmethod
+    def GetVehicleFromMACAddress(MACAddress: str) -> Vehicle:
+        if (MACAddress == PacketLibrary.MRA_MAC_ADDRESS):
             return Vehicle.MRA
-        case "2":
+        elif (MACAddress == PacketLibrary.MEA_MAC_ADDRESS):
             return Vehicle.MEA
-        case "3":
+        elif (MACAddress == PacketLibrary.ERU_MAC_ADDRESS):
             return Vehicle.ERU
-        case _:
+        else:
             return Vehicle.UNKNOWN
+    
+    @staticmethod
+    def SetVehicleMACAddress(VehicleName: Vehicle, MACAddress: str):
+        match (VehicleName):
+            case Vehicle.MRA:
+                PacketLibrary.MRA_MAC_ADDRESS = MACAddress
+            case Vehicle.MEA:
+                PacketLibrary.MEA_MAC_ADDRESS = MACAddress
+            case Vehicle.ERU:
+                PacketLibrary.ERU_MAC_ADDRESS = MACAddress
+            case _:
+                print("Vehicle specification unknown. MAC address assignment ignored")
